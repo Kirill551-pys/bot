3ecf6f0910d7d5af0d860362219df8ba958af2a4
Kirill <taranyuk31@mail.ru>
2026-08-09 22:56:43 +0300

    cheange commits

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// 🔥 Tailwind подключаем через плагин Vite
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(), // Плагин для v4
+  ],
+  server: { host: true, port: 5173 }
 })
